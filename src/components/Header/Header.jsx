4f69a45d552dda0c59054fa3abcf992e27f8ecc6
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2024-12-08 18:44:33 +0600

    make responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
-import { FaHome, FaUserCircle } from "react-icons/fa";
+import { FaBars, FaHome, FaUserCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useGetAdmin } from "../../hooks/useGetAdmin";
+import { useState } from "react";
+import MobileSidebar from "../Mobile/MobileSidebar";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { admin } = useGetAdmin();
   return (
     <header className="w-full sticky top-0 bg-widget z-50 p-3">
-      <nav className="flex items-center justify-end px-8">
+      <nav className="flex items-center justify-between px-8">
+        <div>
+          <FaBars
+            onClick={() => setIsModalOpen((prev) => !prev)}
+            className="block lg:hidden  text-2xl text-white"
+          />
+        </div>
         <div>
           <div className="flex items-center gap-3">
             <Link to={"/"}>
@@ -26,6 +35,15 @@ const Header = () => {
           </div>
         </div>
       </nav>
+
+      {isModalOpen && (
+        <MobileSidebar
+          isOpen={isModalOpen}
+          onClose={setIsModalOpen}
+          key={"mobleSidebar"}
+          className={"h-screen"}
+        />
+      )}
     </header>
   );
 };
--- src/components/Layout/LeftSide.jsx
+++ src/components/Layout/LeftSide.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { layout } from "../../data/layout/layout";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import PropTypes from "prop-types";
 
-const LeftSide = () => {
+const LeftSide = ({ visibleArrow = true, setIsModalOpen = () => {} }) => {
   const { pathname } = useLocation();
   const [isExtend, setIsExtend] = useState(true);
 
@@ -20,16 +21,13 @@ const LeftSide = () => {
       >
         <div className="sticky top-0 bg-widget">
           <div className="w-44 h-[60px] flex flex-col items-center justify-center">
-            <img
-              className="object-fill px-3"
-              src="/logo.png"
-              alt="logo"
-            />
+            <img className="object-fill px-3" src="/logo.png" alt="logo" />
           </div>
         </div>
         <div className="flex flex-col gap-2 h-full">
           {layout.left_side.map(({ name, icon, link }) => (
             <Link
+              onClick={() => setIsModalOpen(false)}
               className={`flex items-center gap-3 text-white ${
                 !isExtend && "gap-6 py-3"
               } px-4 py-2 duration-200 text-sm text-slate ${
@@ -47,7 +45,9 @@ const LeftSide = () => {
       </div>
       <div
         onClick={handleArrow}
-        className="absolute top-[73px] -right-[10px] w-6 h-6 bg-accent rounded-full border flex items-center justify-center z-40 cursor-pointer"
+        className={`absolute top-[73px] -right-[10px] w-6 h-6 bg-accent rounded-full border flex items-center justify-center z-40 cursor-pointer ${
+          !visibleArrow ? "hidden" : "block"
+        }`}
       >
         {isExtend ? (
           <FaAngleLeft className="text-lg text-white" />
@@ -59,4 +59,8 @@ const LeftSide = () => {
   );
 };
 
+LeftSide.propTypes = {
+  visibleArrow: PropTypes.bool,
+  setIsModalOpen: PropTypes.func,
+};
 export default LeftSide;
